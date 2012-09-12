cdffe05237ebf0fca64a42adb44e97e4d89a02c4
Danish Satkut <danish_satkut@hotmail.com>
2012-09-12 16:18:55 +0530

    Added the EmployeeList Page with UI functionality only.

---
+++ SimpleMVVM/Views/EmployeeList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace SimpleMVVM
+{
+    public partial class EmployeeList : Page
+    {
+        public EmployeeList()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
