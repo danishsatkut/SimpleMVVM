351008f4c2d1c20e43116eafae8b6924aaae2c0b
Danish Satkut <danish_satkut@hotmail.com>
2012-09-12 17:39:18 +0530

    Added a base VM class.

---
+++ SimpleMVVM/ViewModels/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace SimpleMVVM.ViewModels
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
