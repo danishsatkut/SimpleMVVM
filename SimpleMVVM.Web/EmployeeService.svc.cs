47099c552d3df32a47109dc395f7c2081e8692ba
Danish Satkut <danish_satkut@hotmail.com>
2012-09-12 16:05:30 +0530

    Implemented the EmployeeService.

---
+++ SimpleMVVM.Web/EmployeeService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.Generic;
+
+namespace SimpleMVVM.Web
+{
+    [ServiceContract(Namespace = "services.web.simplemvvm")]
+    [SilverlightFaultBehavior]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class EmployeeService
+    {
+        [OperationContract]
+        public IList<Employee> GetEmployees()
+        {
+            var context = new AdventureWorksEntities();
+            return context.Employees.ToList();
+        }
+    }
+}
